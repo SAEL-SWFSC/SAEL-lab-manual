1cee0b0200ba6cb27c56e369a2f5a17b1e888b30
Kourtney Burger <113144270+Kourtney-Burger@users.noreply.github.com>
2025-04-22 14:03:46 -0700

    Update GoogleCloudBulkUpload.R
    added functions to only run outside of work hours (6pm to 6am on weekdays, full time on weekends)

--- Scripts/GoogleCloudBulkUpload.R
+++ Scripts/GoogleCloudBulkUpload.R
@@ -1,4 +1,14 @@
-# Bucket to transfer to
+# Check for weekday and hour
+is_weekday = function(x){
+  weekdays = c('Thursday', 'Friday', 'Monday', 'Tuesday', 'Wednesday')
+  return(weekdays(x) %in% weekdays)
+}
+
+is_after_hours = function(x){
+  return(as.integer(format(x,"%H")) < 6 | as.integer(format(x, "%H")) > 18)
+}
+
+# Google  cloud bucket to transfer to
 my_fmc_bucket_name = 'swfsc-1/drifting_recorder/ADRIFT'
 
 # List deployment IDs to copy to bucket
@@ -8,6 +18,12 @@ ADRIFT_deployments <- grep("^ADRIFT_\\d{3}", all_deployments, value = TRUE) #rem
 
 # Copy wav files
 for (folder in ADRIFT_deployments) {
+  #check day and time
+  while (is_weekday(Sys.time()) & !is_after_hours(Sys.time())) {
+    cat(paste('waiting...',Sys.time(), "\n"))
+    Sys.sleep(600) #wait 10 minutes until trying again
+  }
+  
   LocalDir <- file.path(deployment_dir, folder) #local folder with wav files
   
   wav <- list.files(LocalDir, pattern = "\\.wav$", full.names = TRUE) #list of wav files to copy
@@ -17,10 +33,10 @@ for (folder in ADRIFT_deployments) {
   GCP_path <- file.path(my_fmc_bucket_name, GCP_FolderName) # create path to GCP folder
   
   for (file in wav) {
-    string <- paste("gsutil -m cp -r ", file, " ", "gs://", GCP_path, "/", sep="")
+    string <- paste("gsutil -m cp -r ", file, " ", "gs://", GCP_path, "/", sep="") # create command to pass to google cloud sdk command window
     
-    message("Uploading: ", file, " -> ", GCP_path)
+    message("Uploading: ", file, " -> ", GCP_path) # print a progress message in R console
     
-    system(string)
+    system(string) # pass the command for processing and wav file transfer
   }
 }
