04ba55b39ca280f03b4ff6e1d06802ffe6eeaf17
Kourtney Burger <kourtney.burger@noaa.gov>
2025-06-05 16:20:49 -0700

    created function for bulk upload to gcp

--- Scripts/GoogleCloudBulkUpload.R
+++ Scripts/GoogleCloudBulkUpload.R
@@ -1,27 +1,4 @@
-# -----------LIBRARIES-----------
-library(xlsx)
-
-# -----------FRONT END PATHS / DECISIONS-----------
-Local_dir <- "C:/Users/kourtney.burger/Desktop/test" # Path to Local Directory you want to copy FROM
-Cloud_dir <- "swfsc-1/drifting_recorder/kbtest"      # Path to Cloud Directory you want to copy TO
-offHoursCopy <- FALSE                                # TRUE = Copy on nights/weekends, FALSE = copy until complete
-file_type <- NULL                                    # Copy all files in a directory
-# file_type <- c("\\.wav$",                          # Copy files with select extensions
-#                "\\.log.xml$",
-#                "\\.accel.csv$",
-#                "\\.temp.csv$",
-#                "\\.ltsa$",
-#                "\\.flac$"
-# )
-log_file <- paste0(getwd(), "/GCP_transfer_log_", format(Sys.time(), "%Y%m%d_%H%M%S"), ".txt")  # Log file name, with path (used with gsutil -L command). Leave as is, unless you want to change where the log file is saved to. This structure is built to save it to your current working directory
-
-
-# -----------OPTIONAL BUT RECOMMENDED-----------
-# If there are any spaces within your filenames/foldernames, this batch upload will not work
-# We recommend you run this and replace any spaces in your ORIGINAL filenames/foldernames
-# Risk: If you have documents, code that require the exact names (with spaces), you may have issues
-# IF you need to retain your spaces, then consider direct upload via GCP
-
+# ----FUNCTIONS----
 # Short recursive space-to-underscore renamer
 rename_spaces <- function(path = ".") {
   # Get all files and folders, deepest first
@@ -38,70 +15,131 @@ rename_spaces <- function(path = ".") {
   }
 }
 
-rename_spaces(Local_dir)
 
-
-# -----------COPY FILES TO GCP-----------
 # Check for weekday and hour, run between 6 AM and 6 PM on weekdays
-is_weekday = function(x){
+is_weekday <- function(x){
   weekdays = c('Thursday', 'Friday', 'Monday', 'Tuesday', 'Wednesday')
   return(weekdays(x) %in% weekdays)
 }
-is_after_hours = function(x){
+
+is_after_hours <- function(x){
   return(as.integer(format(x,"%H")) < 6 | as.integer(format(x, "%H")) > 18)
 }
 
-# List all the files in the local directory, recursively
-file_type <- paste(file_type, collapse = "|") # combine all file types to one regex (uses or expression)
-good_files <- list.files(Local_dir, pattern = file_type, full.names = TRUE, recursive = TRUE)
 
-# Copy files one by one and create a log
-for (file in good_files) {
-  # check for after hours, will run any time if offHoursCopy = FALSE above
-  if (offHoursCopy) {
-    while (is_weekday(Sys.time()) & !is_after_hours(Sys.time())) {
-      cat(paste('waiting...',Sys.time(), "\n"))
-      Sys.sleep(600) #wait 10 minutes until trying again
+# Copy function
+copy_files_to_GCP <- function(Local_dir, Cloud_dir, file_type = NULL, log_file, offHoursCopy = TRUE) {
+  ## List files ####
+  file_type <- paste(file_type, collapse = "|") # combine all file types to one regex (uses or expression)
+  good_files <- list.files(Local_dir, pattern = file_type, full.names = TRUE, recursive = TRUE)
+  
+  ## Copy files ####
+  for (file in good_files) {
+    # check for after hours, will run any time if offHoursCopy = FALSE above
+    if (offHoursCopy) {
+      while (is_weekday(Sys.time()) & !is_after_hours(Sys.time())) {
+        cat(paste('waiting...',Sys.time(), "\n"))
+        Sys.sleep(600) #wait 10 minutes until trying again
+      }
     }
+    
+    # build a relative GCP path to retain local folder structure
+    rel_path <- gsub(paste0("^", Local_dir, "/?"), "", file)
+    GCP_path <- paste0("gs://", Cloud_dir, "/", rel_path)
+    
+    # create gsutil command
+    cmd <- paste0("gsutil -m cp -c -L \"", log_file, "\" \"", file, "\" \"", GCP_path, "\"")
+    
+    # pass command to process and print progress message in R console
+    message("Uploading: ", file, " -> ", GCP_path)
+    system(cmd)
   }
   
-  # build a relative GCP path to retain local folder structure
-  rel_path <- gsub(paste0("^", Local_dir, "/?"), "", file)
-  GCP_path <- paste0("gs://", Cloud_dir, "/", rel_path)
   
-  # create gsutil command
-  cmd <- paste0("gsutil -m cp -c -L \"", log_file, "\" \"", file, "\" \"", GCP_path, "\"")
+  ## Create Log ####
+  # read in log file created in cmd
+  log_df <- read.csv(log_file, header = TRUE)
   
-  # pass command to process and print progress message in R console
-  message("Uploading: ", file, " -> ", GCP_path)
-  system(cmd)
+  # subset files that successfully transferred and failed 
+  successful_log <- subset(log_df, Result == "OK") 
+  failed_log <- subset(log_df, Result == "ERROR" | Result == "SKIPPED")
+  
+  # number of files that successfully copied and failed 
+  N_files_copied <- nrow(successful_log)
+  N_files_failed <- nrow(failed_log)
+  
+  # number of folders copied 
+  successful_log$Folder <- dirname(successful_log$Source)  
+  num_folders_copied <- length(unique(successful_log$Folder)) 
+  
+  # Total local size and total cloud size 
+  total_local_size <- sum(as.numeric(successful_log$Source.Size), na.rm = TRUE)
+  total_bytes_transferred <- sum(as.numeric(successful_log$Bytes.Transferred), na.rm = TRUE) 
+  
+  # create summary dataframe
+  summary_df <- data.frame(Files_Copied = N_files_copied, N_files_failed, Folders_Copied = num_folders_copied, Total_Local_Size_bytes = total_local_size, Total_Cloud_Size_bytes = total_bytes_transferred)
+  
+  xlsx_file <- sub("\\.txt$", ".xlsx", log_file)
+  
+  # Create a new workbook
+  wb <- createWorkbook()
+  
+  # Add Summary sheet
+  addWorksheet(wb, "Summary")
+  writeData(wb, "Summary", summary_df)
+  
+  # Add Success Files Sheet
+  addWorksheet(wb, "Completed Files")
+  writeData(wb, "Completed Files", successful_log)
+  
+  # Add Failed Files sheet
+  addWorksheet(wb, "Failed Files")
+  writeData(wb, "Failed Files", failed_log)
+  
+  # Save the workbook
+  saveWorkbook(wb, xlsx_file, overwrite = TRUE)
 }
 
 
-# -----------Clean log file and summarize-----------
-# read in log file created in cmd
-log_df <- read.csv(log_file, header = TRUE)
 
-# subset files that successfully transferred and failed 
-successful_log <- subset(log_df, Result == "OK") 
-failed_log <- subset(log_df, Result == "ERROR" | Result == "SKIPPED")
 
-# number of files that successfully copied and failed 
-N_files_copied <- nrow(successful_log)
-N_files_failed <- nrow(failed_log)
 
-# number of folders copied 
-successful_log$Folder <- dirname(successful_log$Source)  
-num_folders_copied <- length(unique(successful_log$Folder)) 
+# ----EXAMPLE WORKFLOW----
+## ----STEP 1: Define parameters----
+library(openxlsx)
+# Path to Local Directory you want to copy FROM
+Local_dir <- "C:/Users/kourtney.burger/Desktop/test" 
+# Path to Cloud Directory you want to copy TO
+Cloud_dir <- "swfsc-1/drifting_recorder/kbtest"      
+# Log file name with path
+log_file <- paste0(getwd(), "/GCP_transfer_log_", format(Sys.time(), "%Y%m%d_%H%M%S"), ".txt")  #  (used with gsutil -L command). Leave as is, unless you want to change where the log file is saved to. This structure is built to save it to your current working directory
+# Copy off hours?
+offHoursCopy <- FALSE  # default is TRUE = Copy only on nights/weekends
+# Copy all files? Choose file type if you do not want copy all
+file_type <- NULL
+# file_type <- c("\\.wav$", "\\.log.xml$", "\\.accel.csv$", "\\.temp.csv$", "\\.ltsa$", "\\.flac$")
 
-# Total local size and total cloud size 
-total_local_size <- sum(as.numeric(successful_log$Source.Size), na.rm = TRUE)
-total_bytes_transferred <- sum(as.numeric(successful_log$Bytes.Transferred), na.rm = TRUE) 
 
-# create summary dataframes and write both to an excel file
-summary_df <- data.frame(Files_Copied = N_files_copied, N_files_failed, Folders_Copied = num_folders_copied, Total_Local_Size_bytes = total_local_size, Total_Cloud_Size_bytes = total_bytes_transferred)
-failed_df <- failed_log[, c("Source", "Destination", "Source.Size", "Bytes.Transferred", "Result")]
+## ----STEP 2: (Optional) Rename files/folders to remove spaces----
+# Spaces within your filenames/foldernames may lead to downstream processing issues
+# We recommend you run this and replace any spaces in your ORIGINAL filenames/foldernames
+# Risk: If you have documents, code that require the exact names (with spaces), you may have issues
+# DO NOT RUN THIS LINE IF YOU NEED TO RETAIN SPACES IN YOUR FILE NAME, these functions will still work
+rename_spaces(Local_dir)
 
-xlsx_file <- sub("\\.txt$", ".xlsx", log_file)
-write.xlsx(summary_df, xlsx_file, sheetName = "Summary", row.names = FALSE)
-write.xlsx(failed_df, xlsx_file, sheetName = "Failed Files", row.names = FALSE, append = TRUE)
+
+## ----STEP 3: Copy files to GCP----
+# progress will be printed to the R console, check the output xlsx file for errors 
+copy_files_to_GCP(
+  Local_dir = Local_dir,
+  Cloud_dir = Cloud_dir,
+  file_type = file_type,
+  log_file = log_file,
+  offHoursCopy = offHoursCopy
+)
+
+
+# --------------Final Notes:--------------
+# - You must have gsutil installed and authenticated on your system
+# - Make sure the GCP bucket exists and you have write permissions
+# - Output Excel log will be saved to your working directory alongside your log txt file, summarizing success/failures
